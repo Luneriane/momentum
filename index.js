1f273c9c8ccc06e8549112b3809561cac2442a37
Luneriane <luneriane@gmail.com>
2019-07-05 14:56:07 +0200

    add quotes

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const monTitre = document.getElementById('Heure');
 const monIcone = document.getElementById('icone');
 const maTemperature = document.getElementById('temperature');
@@ -8,9 +7,20 @@ const monInput = document.getElementById('inputToDo');
 const mesToDo = document.getElementById('maToDoList');
 const monContenu = document.getElementById('contenu');
 
-let monStorage = localStorage.getItem('monNom');
+let nomStorage = localStorage.getItem('monNom');
+
 let maDate = new Date;
 let monHeure = `${maDate.getHours()} : ${maDate.getMinutes()}`;
+
+//récupération de l'api météo
+fetch(`https://api.apixu.com/v1/current.json?key=25aff8fbdd17436eb17103421192305&q=Paris&lang=fr`)
+.then(response => response.json())
+.then(data => {
+    console.log(data);
+    maTemperature.textContent = data.current.temp_c + "°C";
+    monLieu.textContent = data.location.name;
+    monIcone.setAttribute('src', data.current.condition.icon);
+})
 
 //Fonction temps réel
 setInterval(() => {
@@ -27,31 +37,46 @@ setInterval(() => {
 
 }, 10000);
 
+// Citation du jour
+function fetchQuote() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("get", "./citations.json", true);
+    xhr.onreadystatechange = (e) => {
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+            console.log(e.currentTarget.response)
+            let citations = JSON.parse(e.currentTarget.response);
+            insertQuote(citations);
+        }
+    };
+    xhr.send();
+}
+
+function insertQuote(monJson) {
+
+    let myRandom = Math.floor(Math.random()*15);
+    const maCitation = document.getElementById('maCitation');
+    const monAuteur = document.getElementById('monAuteur');
+    
+    maCitation.innerText = monJson[myRandom].citation;
+    monAuteur.innerText = monJson[myRandom].auteur;
+}
+
+fetchQuote();
 
 // récupération du nom
 window.onload = () => { 
-    monNom.value = monStorage;
+    monNom.value = nomStorage;
     monContenu.style.backgroundImage = `url(./images/${maDate.getHours()}.jpg)`;
     monTitre.innerHTML = monHeure;
 } 
 
-// local storage
+// local storage du nom
 monNom.addEventListener('change', () => {
     localStorage.setItem('monNom', monNom.value);
-    monStorage = localStorage.getItem('monNom');
-    monNom.value = monStorage;
-    console.log(monStorage);
+    nomStorage = localStorage.getItem('monNom');
+    monNom.value = nomStorage;
+    console.log(nomStorage);
 })
-
-//récupération de l'api météo
-fetch(`https://api.apixu.com/v1/current.json?key=25aff8fbdd17436eb17103421192305&q=Paris&lang=fr`)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        maTemperature.textContent = data.current.temp_c + "°C";
-        monLieu.textContent = data.location.name;
-        monIcone.setAttribute('src', data.current.condition.icon);
-    })
 
 //TaskList
 monInput.addEventListener('change', () => {
@@ -65,8 +90,8 @@ monInput.addEventListener('change', () => {
     newTask.appendChild(innerTask); //Ajout de la checkbox
     newTask.appendChild(deleteBtn); //Ajout du X
 
-    deleteBtn.innerHTML = "X";
-
+    deleteBtn.innerHTML = "X"; // Contenu bouton Supprimer
+    
     //attribut de la checkbox
     btnTask.setAttribute('type', 'checkbox');
     btnTask.setAttribute('name', 'maValeur');
@@ -87,6 +112,5 @@ monInput.addEventListener('change', () => {
     //suppression task
     deleteBtn.addEventListener('click', () => {
         mesToDo.removeChild(newTask);
-    })
-    
+    })   
 })
